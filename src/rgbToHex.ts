db5568e9ea023e39678e4aa52aa5e35e7675dac9
Daniel Lehr <daniel.lehr@allthings.me>
2019-07-19 09:54:36 +0200

    Clean

--- src/rgbToHex.ts
+++ src/rgbToHex.ts
@@ -1,8 +1,4 @@
-export default function rgbToHex(
-  red: number,
-  green: number,
-  blue: number,
-): string {
+export default function rgbToHex(red: number, green: number, blue: number) {
   const rgb = blue | (green << 8) | (red << 16)
   return '#' + (0x1000000 + rgb).toString(16).slice(1)
 }
